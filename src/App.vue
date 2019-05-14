68523f685245ccae65ac2f6d72173efdf6336ad9
Ewan88 <baileyewan@gmail.com>
2019-05-14 18:23:03 +0100

    adds first background

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Forest/>
+  </div>
+</template>
+
+<script>
+import Forest from './components/Forest.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Forest
+  }
+}
+</script>
+
+<style>
+
+body {
+  margin: 0;
+}
+
+#app {
+  
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
---
+++ src/components/Forest.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div id="background">
+    <img id="back"
+      src="../assets/forest/backTrees.png"
+    />
+    <img id="lights"
+      src="../assets/forest/lights.png"
+    />
+    <img id="middle"
+      src="../assets/forest/middleTrees.png"
+    />
+    <img id="front"
+      src="../assets/forest/frontTrees.png"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'forest',
+}
+</script>
+
+<style lang="css" scoped>
+
+  img {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+
+</style>
